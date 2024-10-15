6f30eb8c886ee07a05c686f15feaa8d9c015d867
viju <avijusanjai@gmail.com>
2024-10-16 00:44:33 +0530

    Revert "bad background"
    This reverts commit 035c496f704c082ed656ea5deb071c3b094c217a.

--- frontend/src/components/Particles.tsx
+++ frontend/src/components/Particles.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
-import type { Engine } from 'tsparticles-engine'; // Ensure you're importing the correct type
 
 const ParticlesBackground: React.FC = () => {
-  const particlesInit = async (main: Engine) => {
+  const particlesInit = async (main: any) => {
     await loadFull(main);
   };
 
