f90047dc14115e824b953555010ef33156842c1d
viju <avijusanjai@gmail.com>
2024-10-16 00:47:33 +0530

    Revert "goofy background"
    This reverts commit f791e7ba11cb4cbc4825b07e3d76ab2f84f3f446.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,19 @@
-// src/App.tsx
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import CodeShare from './pages/CodeShare';
-import Home from './pages/Home';
-import MyFiles from './pages/MyFiles';
+import CodeShare from './pages/CodeShare'; // Import your pages
+import Home from './pages/Home'; // Import your pages
+import MyFiles from './pages/MyFiles'; // Import your pages
 import LocationBasedSaveButton from "./components/LocationBasedSave";
 import LocationBasedDropdown from "./components/LocationBasedButtons";
 import { useState } from 'react';
-import ParticlesBackground from './components/Particles'; // Import the ParticlesBackground
 
 function App() {
   const [position, setPosition] = useState("python");
 
   return (
     <Router>
-      <div className="flex flex-col h-screen bg-slate-700 relative overflow-hidden">
-        {/* Particles Background */}
-        <ParticlesBackground />
-
-        <nav className="bg-gray-900 shadow-md z-10">
+      <div className="flex flex-col h-screen bg-slate-700">
+        <nav className="bg-gray-900 shadow-md">
           <div className="flex justify-between items-center px-6 py-4">
             <h1 className="text-white text-xl font-bold">Share</h1>
             <div className="flex space-x-4">
@@ -42,7 +37,7 @@ function App() {
             </div>
           </div>
         </nav>
-        <div className="flex-grow overflow-hidden z-10"> {/* Ensure the content is on top */}
+        <div className="flex-grow overflow-hidden">
           <Routes>
             <Route path="/" element={<Navigate to="/home" />} />
             <Route path="/codeshare" element={<CodeShare someString={position} />} />
@@ -54,5 +49,4 @@ function App() {
     </Router>
   );
 }
-
 export default App;
