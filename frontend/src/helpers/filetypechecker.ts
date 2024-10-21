eec0c1550b3ede3743fba00428ee418ccaf55dca
viju <avijusanjai@gmail.com>
2024-10-21 18:05:07 +0530

    fixed an undefined ts error

--- frontend/src/helpers/filetypechecker.ts
+++ frontend/src/helpers/filetypechecker.ts
@@ -1,30 +1,30 @@
-function fileTypeChecker(language:string, a:string):string{
-    if (a=='filetype'){if(language==='javascript'){
-      return 'application/javascript';
+function fileTypeChecker(language: string, action: string): string {
+    if (action === 'filetype') {
+      switch (language) {
+        case 'javascript':
+          return 'application/javascript';
+        case 'python':
+          return 'application/x-python';
+        case 'html':
+          return 'text/html';
+        default:
+          return 'text/plain';
+      }
+    } else if (action === 'reverse') {
+      switch (language) {
+        case 'application/javascript':
+          return 'javascript';
+        case 'application/x-python':
+          return 'python';
+        case 'text/html':
+          return 'html';
+        default:
+          return 'txt';
+      }
+    }
+    
+    return '';
   }
-  else if(language==='python'){
-      return 'application/x-python';
-  }
-  else if(language==='html'){
-      return 'text/html';
-  }
-  else{
-      return 'text/plain';
-  }
-  }
-  else if(a=='reverse'){
-        if(language==='application/javascript'){
-            return 'javascript';
-        }
-        else if(language==='application/x-python'){
-            return 'python';
-        }
-        else if(language==='text/html'){
-            return 'html';
-        }
-        else{
-            return 'txt';
-  }
-}}
   
   export default fileTypeChecker;
+  
